5d9c5629fee7bcfd2b3ef5da35af445286bba087
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-07-07 15:57:09 -0700

    Make xtbox.pl able to support multiple databases and have it figure out test failures properly with the new version of Test::Harness that it requires.

--- xtbox.pl
+++ xtbox.pl
@@ -7,17 +7,20 @@ use Tinderbox::Client;
 
 $ENV{BZ_WRITE_TESTS} = 1;
 
+my $db = $ARGV[0] || 'MySQL';
+my $dir = "xtbox-" . lc($db);
+
 my $client = new Tinderbox::Client({
-    Lock      => '.xt-lock',
+    Lock      => ".xt-$db-lock",
     Admin     => 'mkanat@bugzilla.org',
     To        => 'tinderbox-daemon@tinderbox.mozilla.org',
     Tinderbox => 'Bugzilla',
-    Build     => "xt MySQL cg-bugs01",
+    Build     => "xt $db cg-bugs01",
     Commands  => ["./checksetup.pl /home/tinderbox/qa-answers",
                   "$^X -Mlib=lib /home/tinderbox/bin/prove -v xt/ ::"
                   . " --add-custom-fields"],
-    Dir       => 'xtbox',
-    'Failure Strings' => ['DIED', 'FAILED', '^C '],
+    Dir       => $dir,
+    'Failure Strings' => ['DIED', 'FAILED', '^C ', 'Result: FAIL'],
     'Warning Strings' => ['UNEXPECTEDLY SUCCEEDED',
                           'unexpectedly succeeded'],
 });
