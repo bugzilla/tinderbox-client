65383de1809299d4142c7caaab17f683967c6ec8
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-07-08 10:41:40 -0700

    Make xtbox.pl support multiple branches.

--- xtbox.pl
+++ xtbox.pl
@@ -8,13 +8,15 @@ use Tinderbox::Client;
 $ENV{BZ_WRITE_TESTS} = 1;
 
 my $db = $ARGV[0] || 'MySQL';
+my $branch = $ARGV[1] || '';
 my $dir = "xtbox-" . lc($db);
+$dir .= "-$branch" if $branch;
 
 my $client = new Tinderbox::Client({
     Lock      => ".xt-$db-lock",
     Admin     => 'mkanat@bugzilla.org',
     To        => 'tinderbox-daemon@tinderbox.mozilla.org',
-    Tinderbox => 'Bugzilla',
+    Tinderbox => "Bugzilla$branch",
     Build     => "xt $db cg-bugs01",
     Commands  => ["./checksetup.pl /home/tinderbox/qa-answers",
                   "$^X -Mlib=lib /home/tinderbox/bin/prove -v xt/ ::"
