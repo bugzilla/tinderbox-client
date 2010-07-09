162aa7bf5557566503c6d450bda7105b34fe6b61
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-07-08 20:00:17 -0700

    Make the checksetup-pg do a setlocale to the C locale for PostgreSQL 8.3 compatibility.

--- tinderbox-checksetup-pg.pl
+++ tinderbox-checksetup-pg.pl
@@ -4,6 +4,12 @@ use strict;
 use lib '.';
 require 5.0006;
 use Tinderbox::Client;
+use POSIX;
+
+# PostgreSQL 8.3 and above won't let you connect to a SQL_ASCII
+# database if you have a UTF-8 LC_CTYPE. It will allow connecting
+# to any database from the C locale, though.
+POSIX::setlocale(POSIX::LC_CTYPE, 'C');
 
 my $client = new Tinderbox::Client({
     Admin     => 'mkanat@bugzilla.org',
