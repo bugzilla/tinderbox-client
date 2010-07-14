b5a35d4633045ebe37b4190a1786d13803d19a4c
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-07-14 09:55:13 -0700

    Make the perl boxes run install-module before they run checksetup, optionally upgrading all modules.

--- perlbox.pl
+++ perlbox.pl
@@ -3,9 +3,12 @@
 use strict;
 use lib '.';
 require 5.0006;
+use Getopt::Long;
 use Tinderbox::Client;
 
 use constant DEFAULT_PERL => '5.8.8';
+
+GetOptions("upgrade" => \my $do_upgrade) || die $@;
 
 my ($branch, $perl) = @ARGV;
 $perl ||= DEFAULT_PERL;
@@ -15,7 +18,9 @@ $branch_no_dots =~ s/\.//g;
 $perl_no_dots =~ s/\.//g;
 
 my $tinderbox = $branch eq 'tip' ? "Bugzilla" : "Bugzilla$branch";
-my $dir = $perl eq DEFAULT_PERL ? "bugzilla-$branch" : "bugzilla-$branch-$perl_no_dots";
+my $dir = $perl eq DEFAULT_PERL ? "bugzilla-$branch" 
+                                : "bugzilla-$branch-$perl_no_dots";
+my $install_switch = $do_upgrade ? '--upgrade-all' : '--all';
 
 my $client = new Tinderbox::Client({
     Lock      => ".${branch_no_dots}-lock",
@@ -23,7 +28,8 @@ my $client = new Tinderbox::Client({
     To        => 'tinderbox-daemon@tinderbox.mozilla.org',
     Tinderbox => $tinderbox,
     Build     => "perl $perl cg-bugs01",
-    Commands  => ["perl$perl -w ./checksetup.pl --check-modules", 
+    Commands  => ["perl$perl -w install-module.pl $install_switch 2> /dev/null",
+                  "perl$perl -w checksetup.pl --check-modules",
                   "perl$perl -w runtests.pl --verbose"],
     Dir       => $dir,
     'Failure Strings' => ['[checkout aborted]', '--ERROR',
