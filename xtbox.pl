40353555ead1995a066dc1f798acceb685f19785
Max Kanat-Alexander <mkanat@bugzilla.org>
2011-05-16 20:03:55 -0700

    And also make sure the Build is unique.

--- xtbox.pl
+++ xtbox.pl
@@ -11,16 +11,16 @@ $ENV{BZ_WRITE_TESTS} = 1;
 my %switch;
 GetOptions(\%switch, 'long=s');
 
-my $db = $ARGV[0] || 'MySQL';
-my $branch = $ARGV[1] || '';
 my $long = $switch{'long'};
+my $db = $ARGV[0] || 'MySQL';
+$db .= "-$long" if $long;
+my $branch = $ARGV[1] || '';
 my $dir = "xtbox-" . lc($db);
 $dir .= "-$branch" if $branch;
-$dir .= "-$long" if $long;
 my $long_args = $long ? " --long --top-operators=$long" : "";
 
 my $client = new Tinderbox::Client({
-    Lock      => $long ? ".xt-long" : ".xt-$db-lock",
+    Lock      => ".xt-$db-lock",
     Admin     => 'mkanat@bugzilla.org',
     To        => 'tinderbox-daemon@tinderbox.mozilla.org',
     Tinderbox => "Bugzilla$branch",
