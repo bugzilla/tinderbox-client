d4f5eeb69cf0124239356dbb2f20d7550f9daaa3
Max Kanat-Alexander <mkanat@bugzilla.org>
2011-05-16 19:58:05 -0700

    Make it possible for xtbox.pl to run long AND/OR tests with a top operator.

--- xtbox.pl
+++ xtbox.pl
@@ -4,23 +4,29 @@ use strict;
 use lib '.';
 require 5.0006;
 use Tinderbox::Client;
+use Getopt::Long;
 
 $ENV{BZ_WRITE_TESTS} = 1;
+
+my %switch;
+GetOptions(\%switch, 'long=s');
 
 my $db = $ARGV[0] || 'MySQL';
 my $branch = $ARGV[1] || '';
 my $dir = "xtbox-" . lc($db);
 $dir .= "-$branch" if $branch;
+my $long = $switch{'long'};
+my $long_args = $long ? " --long --top-operators=$long" : "";
 
 my $client = new Tinderbox::Client({
-    Lock      => ".xt-$db-lock",
+    Lock      => $long ? ".xt-long" : ".xt-$db-lock",
     Admin     => 'mkanat@bugzilla.org',
     To        => 'tinderbox-daemon@tinderbox.mozilla.org',
     Tinderbox => "Bugzilla$branch",
     Build     => "xt $db cg-bugs01",
     Commands  => ["./checksetup.pl /home/tinderbox/qa-answers",
                   "$^X -Mlib=lib /home/tinderbox/bin/prove -v xt/ ::"
-                  . " --add-custom-fields"],
+                  . " --add-custom-fields$long_args"],
     Dir       => $dir,
     'Failure Strings' => ['DIED', 'FAILED', '^C ', 'Result: FAIL'],
     'Warning Strings' => ['UNEXPECTEDLY SUCCEEDED',
